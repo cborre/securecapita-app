b5c0185d3af6edce0f61c5da2a5c5ca33e4cf77c
Carlos Alberto Borré Antero <cborre2007@gmail.com>
2024-08-19 14:03:07 -0500

    Add downloading invoices as pdf

--- src/app/component/invoice/invoice.component.ts
+++ src/app/component/invoice/invoice.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, ParamMap} from '@angular/router';
+import {Observable, BehaviorSubject, switchMap, map, startWith, catchError, of} from 'rxjs';
+import {DataState} from '../../enum/datastate.enum';
+import {CustomHttpResponse} from '../../interface/appstates';
+import {Customer} from '../../interface/customer';
+import {Invoice} from '../../interface/invoice';
+import {State} from '../../interface/state';
+import {User} from '../../interface/user';
+import {CustomerService} from '../../service/customer.service';
+import {jsPDF as pdf} from 'jspdf';
+
+const INVOICE_ID = 'id';
 
 @Component({
   selector: 'app-invoice',
   templateUrl: './invoice.component.html',
   styleUrl: './invoice.component.css'
 })
-export class InvoiceComponent {
+export class InvoiceComponent implements OnInit {
+  invoiceState$: Observable<State<CustomHttpResponse<Customer & Invoice & User>>>;
+  private dataSubject = new BehaviorSubject<CustomHttpResponse<Customer & Invoice & User>>(null);
+  private isLoadingSubject = new BehaviorSubject<boolean>(false);
+  isLoading$ = this.isLoadingSubject.asObservable();
+  readonly DataState = DataState;
 
+  constructor(private activatedRoute: ActivatedRoute, private customerService: CustomerService) {
+  }
+
+  ngOnInit(): void {
+    this.invoiceState$ = this.activatedRoute.paramMap.pipe(
+      switchMap((params: ParamMap) => {
+        return this.customerService.invoice$(+params.get(INVOICE_ID))
+          .pipe(
+            map(response => {
+              console.log(response);
+              this.dataSubject.next(response);
+              return {dataState: DataState.LOADED, appData: response};
+            }),
+            startWith({dataState: DataState.LOADING}),
+            catchError((error: string) => {
+              return of({dataState: DataState.ERROR, error})
+            })
+          )
+      })
+    );
+  }
+
+  exportAsPDF(): void {
+    const filename = `invoice-${this.dataSubject.value.data['invoice'].invoiceNumber}.pdf`;
+    const doc = new pdf();
+    doc.html(document.getElementById('invoice'), {
+      margin: 5, windowWidth: 1000, width: 200,
+      callback: (invoice) => invoice.save(filename)
+    });
+  }
 }
